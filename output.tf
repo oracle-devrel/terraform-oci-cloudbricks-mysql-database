0ecee9b9a9c89692a66f3c412aabca7f32bd3693
Nuno Gonçalves <nuno.goncalves@oracle.com>
2022-02-21 11:02:17 +0100

    variable defaults and output tweak

--- output.tf
+++ output.tf
@@ -4,10 +4,7 @@
 #
 # Purpose: The following script defines the output for the mysql database system
 
-
-
 output "mysql_db_system" {
   description = "MySQL Database DBSystem"
   value       = oci_mysql_mysql_db_system.DBSystem
-  sensitive   = true
 }
